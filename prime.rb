b2592c47ad2b8c310123e1d169d89122696b03e7
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-10-15 15:34:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,5 @@
 def prime?(num)
-  return false if num <= 1
+  return false if (num <= 1) || (num === 4)
   return true if (num === 2) || (num === 3)
-
-  (2..num-1).all? do |n|
-    if n.odd?
-      if num % n === 0
-        return true
-      end
-    else
-      return false
-    end
-  end
+  (2..num-1).all? { |n| (n.odd?) && (num % n === 0) ? false  : true }
 end
