4907819f880ae13deec400451c02e98fc42d9ef7
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-10-15 15:22:35 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(num)
   return false if num <= 1
   return true if (num === 2) || (num === 3)
 
-  (2..num-1).all? do |n|  
+  (2..num-1).all? do |n|
     if n.odd?
       if num % n === 0
         return false
